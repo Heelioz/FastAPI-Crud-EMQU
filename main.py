14b56b343653db8f7ff7257aa3e527a3389e8927
Helioz <mjduque.19@est.ucab.edu.ve>
2024-04-17 21:23:58 -0400

    Authetication Fix

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from Routes import users
-from Routes import pc
+from routers import pc
+from routers import basic_auth_users
 from pydantic import BaseModel
+
 
 app = FastAPI()
 
 #Routes
-app.include_router(users.router)
+
 app.include_router(pc.router)
+app.include_router(basic_auth_users.router)
